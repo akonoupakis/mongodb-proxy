4676980affff317cd0760d33b5d2e4efc3926ae3
Apostolis Konoupakis <konoupakis@gmail.com>
2016-03-22 00:30:45 +0200

    removed dev bits

--- dev/collections/users/post.js
+++
@@ -1,6 +0,0 @@
-﻿module.exports = function (sender, context, data) {
-
-    console.log('users post', context.previous, context.data, data);
-    context.done();
-    
-}
--- dev/collections/users/validate.js
+++
@@ -1,7 +0,0 @@
-﻿module.exports = function (sender, context, data) {
-
- //   console.log('users put', data);
-    console.log('users validate', context.previous, context.data, data);
-    context.done();
-    
-}
